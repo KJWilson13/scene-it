5be644c7702e5c910e6491939d335104bd37f62a
Kenneth Wilson <kjwilson9779@gmail.com>
2022-08-30 20:43:53 -0400

    First Push

---
+++ index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // code here will execute after the document is loaded
+});
+
+function renderMovies(movieArray) {
+    const movieHtmlArray =  movieArray.map((currentMovie) => {
+       
+    });
+    return movieHtmlArray.join('');
+}
+
