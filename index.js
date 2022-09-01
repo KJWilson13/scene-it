275375bbc791dfffabc8909f44bdc9140cab66f5
Kenneth Wilson <kjwilson9779@gmail.com>
2022-09-01 18:55:07 -0400

    Part One

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
+const myForm = document.getElementById('search-form');
+const movieContainer = document.getElementById("moviesContainer")
+
 document.addEventListener('DOMContentLoaded', function() {
     // code here will execute after the document is loaded
+    myForm.addEventListener('submit', function(e){
+        // event listener code goes here
+        e.preventDefault();
+        console.log("argh");
+        movieContainer.innerHTML = renderMovies(movieData);
+        
+    })
 });
+
 
 function renderMovies(movieArray) {
     const movieHtmlArray =  movieArray.map((currentMovie) => {
-       
+        return `<div>
+        <img class="card-img-top" src=${currentMovie.Poster}/>
+        <h2>${currentMovie.Title}</h2>
+        <p>${currentMovie.Year}</p>
+    
+        <button class="add-button" data-imdbid="${currentMovie.imdbID}">Add Movie</button>
+        </div>`
     });
     return movieHtmlArray.join('');
 }
-
